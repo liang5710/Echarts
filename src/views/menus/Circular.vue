92bbf89d3ab6fcae481a8116f1ce95d7bb7ce00f
unknown <727660735@qq.com>
2019-05-29 10:16:43 +0800

    部分代码优化

--- src/views/menus/Circular.vue
+++ src/views/menus/Circular.vue
@@ -1,92 +1,97 @@
 <template>
-     <div id="myChart" :style="{width:'100%',height:'600px'}"></div>
+  <div id="myChart" :style="{ width: '100%', height: '600px' }"></div>
 </template>
 
 <script>
-import _ from 'lodash'
-import echarts from 'echarts'
+import _ from "lodash";
+import echarts from "echarts";
 export default {
-  data(){
-      return {
-      }
+  data() {
+    return {};
   },
-  mounted(){
+  mounted() {
     this.drawLine();
   },
   methods: {
-    drawLine(){
-        // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById('myChart'))
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("myChart"));
 
       myChart.setOption({
-        title:{   //标题
-                text:'环形报表',   //主标题
-                subtext:'虚构数据',    //子标题
-                x:'left',             //x轴位置，默认 left
-                y:'',                 //y轴位置，默认 top
-                top:20
-            },
-        backgroundColor:'#fff', //背景颜色
+        title: {
+          //标题
+          text: "环形降雨量报表", //主标题
+          subtext: "虚构数据", //子标题
+          x: "left", //x轴位置，默认 left
+          y: "", //y轴位置，默认 top
+          top: 20
+        },
+        backgroundColor: "#fff", //背景颜色
         tooltip: {
-          formatter:"{a}<br/>{b}:{c}({d}%)" //自定义显示格式
+          formatter: "{a}<br/>{b}:{c}ml({d}%)" //自定义显示格式
         },
-        legend:{
-          orient:'horizontal',  //vertical,horizontal 纵向横向
-          left:'center',
-          bottom: 10,  //距离底部 top,left right 都可以设置
+        legend: {
+          orient: "horizontal", //vertical,horizontal 纵向横向
+          left: "center",
+          bottom: 10 //距离底部 top,left right 都可以设置
         },
-        toolbox: { //可用于右侧图标用于  查看数据视图，编辑，保存图表图片
+        toolbox: {
+          //可用于右侧图标用于  查看数据视图，编辑，保存图表图片
+          show: true,
+          orient: "vertical",
+          x: "right",
+          y: "center",
+          feature: {
+            mark: { show: false },
+            dataView: { show: false, readOnly: true },
+            restore: { show: false },
+            saveAsImage: { show: true }
+          }
+        },
+        series: [
+          {
+            name: "降雨量",
+            type: "pie", //图表类型
+            radius: ["50%", "70%"],
+            label: {
+              normal: {
+                show: false,
+                position: "center",
+                formatter: "{b}:{c}ml({d}%)"
+              },
+              emphasis: {
+                //中间显示内容
                 show: true,
-                orient : 'vertical',
-                x: 'right',
-                y: 'center',
-                feature : {
-                    mark : {show: false},
-                    dataView : {show: false, readOnly: true},
-                    restore : {show: false},
-                    saveAsImage : {show: true}
-                }
-            },
-        series: [{
-            name: '降雨量',
-            type: 'pie',   //图表类型
-            radius:['50%','70%'],
-            label:{
-              normal:{
-                show:false,
-                position:'center'
-              },emphasis:{  //中间显示内容
-                show:true,
-                textStyle:{
-                  fontSize:30,
-                  fontWeight:'bold'
+                textStyle: {
+                  fontSize: 20,
+                  fontWeight: "bold"
                 }
               }
             },
             data: []
-        }]
-    });
+          }
+        ]
+      });
 
-    myChart.showLoading();
-    this.$http.post('api/getJYL')
-        .then((res)=>{
-          let xdata= _.map(res.data,(item)=>{
-                    return item.name
-                  })
+      myChart.showLoading();
+      this.$http.post("api/getJYL").then(res => {
+        let xdata = _.map(res.data, item => {
+          return item.name;
+        });
 
-           myChart.hideLoading();
-           myChart.setOption({
-             legend: {
-                data:xdata
-             },
-             series:{
-               data:res.data
-             }
-           })
-        })
+        myChart.hideLoading();
+        myChart.setOption({
+          legend: {
+            data: xdata
+          },
+          series: {
+            data: res.data
+          }
+        });
+      });
     }
   }
-}
+};
 </script>
 
 
